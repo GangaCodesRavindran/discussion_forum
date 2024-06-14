141bdd4539859b7c80f94a5ebdc3388fa803b153
Ganga Ravindran <ganga.ravindran@epiroc.com>
2024-06-14 10:07:45 +0530

    Update style sheets

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import QuestionForm, AnswerForm
 from .models import Question, Answer
-
+'''
 def home(request):
     questions = Question.objects.all()
     form = QuestionForm()
@@ -22,6 +22,8 @@ def question_detail(request, pk):
     else:
         form = AnswerForm()
     return render(request, 'question_detail.html', {'question': question, 'form': form})
+'''
+
 
 @login_required
 def post_question(request):
@@ -50,5 +52,31 @@ def landing(request):
 
 def library(request):
     return render(request, 'library.html')
+
+
+
+def home(request):
+    questions = Question.objects.all()
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = QuestionForm()
+    return render(request, 'home.html', {'questions': questions, 'form': form})
+
+def question_detail(request, pk):
+    question = get_object_or_404(Question, pk=pk)
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.question = question
+            answer.save()
+            return redirect('question_detail', pk=pk)
+    else:
+        form = AnswerForm()
+    return render(request, 'question_detail.html', {'question': question, 'form': form})
 
 
