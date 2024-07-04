ada18b32acf9522329a6ed581e8553c4d3a9373e
Ganga Ravindran <ganga.ravindran@epiroc.com>
2024-07-04 14:14:37 +0530

    first try working with profile info on each page

--- project_lms/settings.py
+++ project_lms/settings.py
@@ -97,9 +97,9 @@ WSGI_APPLICATION = 'project_lms.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'my_database',
+        'NAME': 'maindb',
         'USER': 'postgres',
-        'PASSWORD': 'root',
+        'PASSWORD': '1234',
         'HOST': 'localhost',
         'PORT': '5432',
     }
