05c96872c703bb25745d69c1f84e5af5e52c7a21
Ganga Ravindran <ganga.ravindran@epiroc.com>
2024-07-10 09:09:30 +0530

    The question and answer can be updated and deleted

--- disc_forum/urls.py
+++ disc_forum/urls.py
@@ -1,8 +1,13 @@
+# disc_forum\urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('forum/', views.forum, name='forum'),
     path('forum/<int:pk>/', views.question_detail, name='question_detail'),
+    path('delete_question/<int:pk>/', views.delete_question_detail, name='delete_question_detail'),
+    path('update_question/<int:pk>/', views.update_question_detail, name='update_question_detail'),
+    path('delete_answer/<int:pk>/', views.delete_answer, name='delete_answer'),
+    path('update_answer/<int:pk>/', views.update_answer, name='update_answer'),
     path('post_question/', views.post_question, name='post_question'),
 ]
--- disc_forum/views.py
+++ disc_forum/views.py
@@ -27,6 +27,26 @@ def post_question(request):
     return render(request, 'disc_forum/post_question.html', {'form': form})
 
 @login_required
+def delete_question_detail(request, pk):
+    question = get_object_or_404(Question, pk=pk)
+    if request.method == 'POST':
+        question.delete()
+        return redirect('forum')
+    return render(request, 'disc_forum/confirm_delete.html', {'object': question, 'type': 'question'})
+
+@login_required
+def update_question_detail(request, pk):
+    question = get_object_or_404(Question, pk=pk)
+    if request.method == 'POST':
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            form.save()
+            return redirect('question_detail', pk=pk)
+    else:
+        form = QuestionForm(instance=question)
+    return render(request, 'disc_forum/update_question.html', {'form': form, 'question': question})
+
+@login_required
 def forum(request):
     questions = Question.objects.all()
     if request.method == 'POST':
@@ -51,3 +71,25 @@ def question_detail(request, pk):
     else:
         form = AnswerForm()
     return render(request, 'disc_forum/question_detail.html', {'question': question, 'form': form})
+
+@login_required
+def delete_answer(request, pk):
+    answer = get_object_or_404(Answer, pk=pk)
+    question_pk = answer.question.pk
+    if request.method == 'POST':
+        answer.delete()
+        return redirect('question_detail', pk=question_pk)
+    return render(request, 'disc_forum/confirm_delete.html', {'object': answer, 'type': 'answer'})
+
+@login_required
+def update_answer(request, pk):
+    answer = get_object_or_404(Answer, pk=pk)
+    question_pk = answer.question.pk
+    if request.method == 'POST':
+        form = AnswerForm(request.POST, instance=answer)
+        if form.is_valid():
+            form.save()
+            return redirect('question_detail', pk=question_pk)
+    else:
+        form = AnswerForm(instance=answer)
+    return render(request, 'disc_forum/update_answer.html', {'form': form, 'answer': answer})
